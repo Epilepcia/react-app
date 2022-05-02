09d8f83c989f203f207644f4f39a70c4a4c79b62
Epilepcia <arsgold23072@gmail.com>
2022-05-02 16:58:43 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="http://cdn.shopify.com/s/files/1/0501/0043/7159/files/ESOTERICA_LOGO22_1200x1200.png?v=1643338666" />
+    </header>
+  );
+};
+
+export default Header;
